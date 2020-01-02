a7942fee07df2a2e5126a9c0c504fb0691afb04f
jorge0102 <49454085+jorge0102@users.noreply.github.com>
2020-01-02 20:03:22 +0100

    Update keys.js

--- LoginGoogle/config/keys.js
+++ LoginGoogle/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    googleClientID: '449619391263-hfev0lqpc23u37r56bda7dco5s0t1j9h.apps.googleusercontent.com',
-    googlelientSecret: 'EegDFRXURL7TSn_jUfWvUbot',
+    googleClientID: '152626+5ergersyh6d5nty+', // Aqui ira vuestra clave de cliente de la Api ,son claves inventadas estas 2
+    googlelientSecret: 'EegDFRX', // Aqui ira la clave secreta que os da la API
     cookieKey: '12ohbhvyg3f165e39YUZGaGBBkKBSABHCY98364098KFANF'
 };
